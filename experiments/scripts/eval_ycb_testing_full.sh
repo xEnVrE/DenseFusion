85fa24fc483cc4e6d2fb86b5a3432e766b07e8e1
Nicola Piga <nicolapiga@gmail.com>
2019-09-07 09:40:38 +0200

    Create results folders in script experiments/scripts/eval_ycb_testing_full.sh

--- experiments/scripts/eval_ycb_testing_full.sh
+++ experiments/scripts/eval_ycb_testing_full.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+# set -x
 set -e
 
 export PYTHONUNBUFFERED="True"
@@ -14,6 +14,14 @@ if [ ! -d YCB_Video_toolbox ];then
     cd ..
     cp replace_ycb_toolbox/*.m YCB_Video_toolbox/
 fi
+
+for class_name in `cat ./YCB_Video_toolbox/classes.txt`
+do
+    for i in 0048 0049 0050 0051 0052 0053 0054 0055 0056 0057 0058 0059
+    do
+        mkdir -p experiments/eval_result/ycb/${class_name}/${i}
+    done
+done
 
 python3 ./tools/eval_ycb_testing_full.py --dataset_root ./datasets/ycb/YCB_Video_Dataset\
   --model trained_checkpoints/ycb/pose_model_26_0.012863246640872631.pth\
