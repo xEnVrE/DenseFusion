30a2f8f4051cbf14f52c630a5e59fdc4aa0eca6c
Nicola Piga <nicolapiga@gmail.com>
2020-07-05 00:57:28 +0200

    Add evaluation of velocity with finite differences in tools/yarp_module.py

--- tools/yarp_module.py
+++ tools/yarp_module.py
@@ -1,9 +1,11 @@
 import argparse
+import math
 import numpy
 import os
 import sys
 import time
 import yarp
+from pyquaternion import Quaternion
 
 sys.path.insert(0, os.getcwd())
 from lib.inference import Inference
@@ -41,7 +43,15 @@ class InferenceModule (yarp.RFModule):
         self.prediction_out = yarp.Port()
         self.prediction_out.open("/dense-fusion/pose:o")
 
-        # Inumpyut buffers initialization
+        # Initialize gaze controller
+        props = yarp.Property()
+        props.put("device", "gazecontrollerclient")
+        props.put("local", "/densefusion/gaze")
+        props.put("remote", "/iKinGazeCtrl")
+        self.gaze_driver = yarp.PolyDriver(props)
+        self.gaze = self.gaze_driver.viewIGazeControl()
+
+        # Input buffers initialization
         self.rgb_buffer = bytearray(numpy.zeros((self.options.height, self.options.width, 3), dtype = numpy.uint8))
         self.rgb_image = yarp.ImageRgb()
         self.rgb_image.resize(self.options.width, self.options.height)
@@ -56,6 +66,10 @@ class InferenceModule (yarp.RFModule):
         self.mask_image = yarp.ImageMono()
         self.mask_image.resize(self.options.width, self.options.height)
         self.mask_image.setExternal(self.mask_buffer, self.options.width, self.options.height)
+
+        # Stamp initialization for finite differences
+        self.last_stamp = -1
+        self.last_total_position = None
 
     def close(self):
 
@@ -72,13 +86,19 @@ class InferenceModule (yarp.RFModule):
 
     def updateModule(self):
 
-        start_time = time.time()
-        
-        rgb = self.rgb_in.read(False)
+        # synchronize on rgb stream
+        rgb = self.rgb_in.read(True)
         depth = self.depth_in.read(False)
         mask = self.mask_in.read(False)
 
         if (rgb is not None) and (depth is not None) and (mask is not None):
+
+            eye_position = yarp.Vector()
+            eye_orientation = yarp.Vector()
+            self.gaze.getLeftEyePose(eye_position, eye_orientation)
+            camera_position = numpy.array([eye_position[0], eye_position[1], eye_position[2]])
+            camera_axis = numpy.array([eye_orientation[0], eye_orientation[1], eye_orientation[2]])
+            camera_angle = eye_orientation[3]
 
             self.rgb_image.copy(rgb)
             rgb_frame = numpy.frombuffer(self.rgb_buffer, dtype=numpy.uint8).reshape(self.options.height, self.options.width, 3)
@@ -92,19 +112,56 @@ class InferenceModule (yarp.RFModule):
             prediction = self.inference.inference(self.options.object_id, rgb_frame, depth_frame, mask_frame)
 
             if len(prediction) != 0:
+
+                object_orientation = Quaternion(axis=[prediction[3], prediction[4], prediction[5]], angle = prediction[6])
+                camera_orientation = Quaternion(axis=camera_axis, angle=camera_angle).rotation_matrix
+                total_orientation =  camera_orientation.dot(object_orientation.rotation_matrix)
+                total_orientation_axis = Quaternion(matrix=total_orientation).axis
+                total_orientation_angle = Quaternion(matrix=total_orientation).angle
+                total_position = camera_position + camera_orientation.dot(prediction[0:3])
+
+                stamp = yarp.Stamp()
+                self.rgb_in.getEnvelope(stamp)
+                new_stamp = stamp.getTime()
+
+                velocity = [0, 0, 0, 0, 0, 0]
+                if self.last_stamp > 0:
+                    elapsed =  new_stamp - self.last_stamp
+                    velocity[0:3] = (total_position - self.last_total_position) / elapsed
+
+                    relative_orientation = total_orientation.dot(self.last_total_orientation.transpose())
+                    acos_argument = (relative_orientation.trace() - 1) / 2.0
+                    if acos_argument < -1.0:
+                        acos_argument = -1.0
+                    else:
+                        acos_argument = 1.0
+                    theta = math.acos(acos_argument) + sys.float_info.min
+                    ang_velocity_skew = 1.0 / (2.0 * elapsed) * theta / math.sin(theta) * (relative_orientation - relative_orientation.transpose())
+                    velocity[3:6] = [-ang_velocity_skew[1, 2], ang_velocity_skew[0, 2], -ang_velocity_skew[0, 1]]
+
+                self.last_stamp = new_stamp
+                self.last_total_position = total_position
+                self.last_total_orientation = total_orientation
+
                 output_vector = yarp.Vector()
-                output_vector.resize(7)
-                output_vector[0] = prediction[0]
-                output_vector[1] = prediction[1]
-                output_vector[2] = prediction[2]
-                output_vector[3] = prediction[3]
-                output_vector[4] = prediction[4]
-                output_vector[5] = prediction[5]
-                output_vector[6] = prediction[6]
+                output_vector.resize(13)
+                output_vector[0] = total_position[0]
+                output_vector[1] = total_position[1]
+                output_vector[2] = total_position[2]
+                output_vector[3] = total_orientation_axis[0]
+                output_vector[4] = total_orientation_axis[1]
+                output_vector[5] = total_orientation_axis[2]
+                output_vector[6] = total_orientation_angle
+                output_vector[7] = velocity[0]
+                output_vector[8] = velocity[1]
+                output_vector[9] = velocity[2]
+                output_vector[10] = velocity[3]
+                output_vector[11] = velocity[4]
+                output_vector[12] = velocity[5]
 
                 self.prediction_out.write(output_vector)
-            
-            print(1.0 / (time.time() - start_time))
+
+            # print(1.0 / (time.time() - start_time))
 
         return True
 
